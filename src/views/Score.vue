edc9789490a53c8216c4a8a8087d3187207bf793
Sojin Kim <sosojinny@KazuhironoiMac.local>
2020-06-24 19:06:46 +0900

    0624_firstcommit

---
+++ src/views/Score.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="score">
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+
+<script>
+export default{
+  computed:{
+    count(){
+      return this.$store.getters.count
+    }
+  }
+}
+</script>
